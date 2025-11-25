6e8ee1507c5abdef74dafed38f24da76d9df1fd3
Abdelwahab Mohamed <abdelwahab@global-energy.com>
2025-11-25 09:29:40 +0200

    intialize the project

---
+++ front-end/TMS/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className=''>
+        dls,g'dsgj
+      </div>
+    </>
+  )
+}
+
+export default App
