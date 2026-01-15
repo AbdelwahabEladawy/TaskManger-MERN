e2fd956c7f50f1e066661fd39ea425fa9e82e782
Abdelwahab Mohamed <abdelwahab@global-energy.com>
2026-01-15 14:48:30 +0200

    initialize docker file

--- back-end/server.js
+++ back-end/server.js
@@ -54,7 +54,7 @@ app.use("/api/v1/auth", reportRoutes)
 
 
 
-const PORT = process.env.PORT|| 5000
+const PORT = process.env.PORT|| 8000
 
 app.listen(PORT, () => {
     console.log(`server is running on ${PORT}`);
